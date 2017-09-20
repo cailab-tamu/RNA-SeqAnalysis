de15b1e3f889e4b5a5c7856fb20b29d57684d9e3
dosorio <dcosorioh@tamu.edu>
2017-09-20 13:12:10 -0500

    Update scRNA-Seq
    - bias options added
    -UMI removed

--- scRNA-Seq.R
+++ scRNA-Seq.R
@@ -15,15 +15,14 @@ files <- list.files(args[1])
 # gencodeIndex
 genCode <- args[2]
 
-# UMI
-UMI <- gsub("[[:punct:]]","",gsub("^\\-[[:alpha:]]+","",gsub("[[:digit:]]","",gsub("[[:lower:]]","", files))))
-write(paste0(">","umi_",UMI,"\n",UMI), file = genCode, append = TRUE)
-
 # Salmon Index
 system(paste0("salmon index -t ", genCode, " -i genecodeIndex"))
 
 # SALMON
 files <- list.files(args[1],full.names = TRUE)
 for (file in files){
-  system(paste0("salmon quant -i genecodeIndex -l SR -p 8 -r <(gunzip -c ", file , ") " , "-o ", file ))
+  command <- paste0("salmon quant -i genecodeIndex -l A -p 8 -r <(gunzip -c ", file , ") " , "-o ", basename(file), " --seqBias --gcBias --posBias -g", args[3])
+  writeLines(command, ".command.sh")
+  system("bash .command.sh")
 }
+
