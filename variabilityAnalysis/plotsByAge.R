4d45b2dcf45328e11266055ae9d6e397d04e2a86
dosorio <dcosorioh@tamu.edu>
2017-11-01 12:53:12 -0500

    Update plotsByAge

--- variabilityAnalysis/plotsByAge.R
+++ variabilityAnalysis/plotsByAge.R
@@ -5,10 +5,16 @@ geneAge <- lapply(geneAge,function(gene){gene[c(1,length(gene))]})
 geneAge <- do.call(rbind.data.frame,geneAge)
 colnames(geneAge) <- c("ENSEMBL","AGE")
 dataT <- read.csv("dataFiles/dataT.csv")
-age <- dataT[dataT[,"ENSEMBL"]%in%as.vector(geneAge[geneAge[,"AGE"]%in%c(0),1]),c("RNAseq_CV","scRNAseq_CV")]
-noAge <- dataT[dataT[,"ENSEMBL"]%in%as.vector(geneAge[geneAge[,"AGE"]%in%c(1),1]),c("RNAseq_CV","scRNAseq_CV")]
-plot(noAge$scRNAseq_CV,log(1+noAge$RNAseq_CV), pch = 16, col="gray90", xlab = "(CV) scRNA-seq", ylab = "log(CV) RNA-seq")
-points(age[,2],age[,1], pch=16)
+par(mfrow=c(2,4))
+nameLevel <- c("Human", "Euarchontoglire", "Amniota", "Euteleostomi", "Bilateria", "Opisthokonta", "Eukaryota","Cellular Organisms")
+pdf("plotsByAge/plotByAge.pdf")
+for(i in (seq_len(8)-1)){
+  age <- dataT[dataT[,"ENSEMBL"]%in%as.vector(geneAge[geneAge[,"AGE"]%in%c(0),1]),c("RNAseq_CV","scRNAseq_CV")]
+  noAge <- dataT[dataT[,"ENSEMBL"]%in%as.vector(geneAge[geneAge[,"AGE"]%in%c(i),1]),c("RNAseq_CV","scRNAseq_CV")]
+  plot(noAge$scRNAseq_CV,log(1+noAge$RNAseq_CV), pch = 16, col="gray75", xlab = "(CV) scRNA-seq", ylab = "log(CV) RNA-seq", ylim=c(0,1), main = nameLevel[i+1])
+  points(age[,2],log(1+age[,1]), pch=16)
+}
+dev.off()
 cor(dataT$RNAseq_CV, dataT$scRNAseq_CV)
 cor(age$RNAseq_CV,age$scRNAseq_CV)
 cor.test(noAge$RNAseq_CV,noAge$scRNAseq_CV)
